05ea75832b195d2b39981f08b52a8be97ca0b2d0
Daniele-SS <dev.daniele@outlook.com>
2026-02-02 11:16:16 -0300

    cores traduzidas

--- main.js
+++ main.js
@@ -2,9 +2,27 @@
 
 const botaoTrocarCor = document.getElementById('trocar-cor')
 
+const cores = {
+    "azul": "blue",
+    "vermelho": "red",
+    "verde": "green",
+    "amarelo": "yellow",
+    "preto": "black",
+    "branco": "white",
+    "cinza": "gray",
+    "roxo": "purple",
+    "rosa": "pink",
+    "laranja": "orange",
+    "marrom": "brown",
+    "ciano": "cyan"
+}
+
 function trocarCor(){
-    const cor = document.getElementById('cor').value
-    document.documentElement.style.setProperty('--cor-bg', cor)
+    const inputCor = document.getElementById('cor').value.toLowerCase().trim()
+
+    const corFinal = cores[inputCor] || inputCor
+
+    document.body.style.backgroundColor = corFinal
 }
 
 botaoTrocarCor.addEventListener('click', trocarCor)
