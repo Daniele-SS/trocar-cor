722c4bc583c503e63e680491e917b2ade9a7419b
Daniele-SS <dev.daniele@outlook.com>
2026-02-02 10:47:14 -0300

    projeto finalizado

---
+++ main.js
@@ -0,0 +1,10 @@
+'use stric'
+
+const botaoTrocarCor = document.getElementById('trocar-cor')
+
+function trocarCor(){
+    const cor = document.getElementById('cor').value
+    document.documentElement.style.setProperty('--cor-bg', cor)
+}
+
+botaoTrocarCor.addEventListener('click', trocarCor)
